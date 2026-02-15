d584440c4abae9e80212cc63a2ed5a994dacfd2f
mazensaid-02 <mazenbensaid548@gmail.com>
2026-02-15 17:27:25 +0100

    test

---
+++ hello-odoo/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Hello Odoo',
+    'version': '1.0',
+    'summary': 'Module de test CI/CD',
+    'description': 'Un module simple pour tester le pipeline CI/CD sur Odoo',
+    'author': 'Mazen Said sa ',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
---
+++ hello-odoo/models/hello.py
@@ -0,0 +1,8 @@
+from odoo import models, fields
+
+class HelloOdoo(models.Model):
+    _name = 'hello.odoo'
+    _description = 'Hello Odoo from Sir Mazen'
+
+    name = fields.Char(string='Name', required=True)
+    message = fields.Text(string='Message', default='Hello Mazen')
