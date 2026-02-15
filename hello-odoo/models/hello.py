ee00607965372e0071ae65df0d45cb4cbb3d5360
mazensaid-02 <mazenbensaid548@gmail.com>
2026-02-15 17:33:41 +0100

    test

--- hello-odoo/models/hello.py
+++ hello-odoo/models/hello.py
@@ -2,7 +2,7 @@ from odoo import models, fields
 
 class HelloOdoo(models.Model):
     _name = 'hello.odoo'
-    _description = 'Hello Odoo from Sir Mazen'
+    _description = 'Hello Odoo from Mazen'
 
     name = fields.Char(string='Name', required=True)
     message = fields.Text(string='Message', default='Hello Mazen')
